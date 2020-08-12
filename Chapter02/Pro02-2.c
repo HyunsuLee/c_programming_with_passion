58fd461acaba5f85871af828a12a64ecebbc92ad
HyunsuLee <hyunspookey@gmail.com>
2020-08-12 23:29:01 +0900

    complete chapter02

---
+++ Chapter02/Pro02-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 문제1
+    printf("제 이름은 이현수입니다.\n");
+    printf("제 나이는 %d살이고요.\n", 38);
+    printf("제가 사는 곳의 번지수는 %d-%d입니다.\n", 123, 456);
+    // 문제2
+    printf("%dx%d=%d\n", 4, 5, 4*5);
+    printf("%dx%d=%d\n", 7, 9, 7*9);
+    return 0;
+}
